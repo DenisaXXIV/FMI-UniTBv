08d03eb179e1a55fa382010889db807838285c3a
Vasile Denisa-Georgiana <vasile.denisa.24@gmail.com>
2023-06-04 01:54:14 +0300

    Added year2/SMM/Lab2

---
+++ Year_2/Semester_II/SMM - Modern 3D Graphic Modeling Systems/Labs/Lab2/Lab2 - Hello DirectX 11/Lab2 - Hello DirectX 11/Lab2 - Hello DirectX 11.cpp
@@ -0,0 +1,446 @@
+﻿#include "framework.h"
+#include "Lab2 - Hello DirectX 11.h"
+
+#include <windowsx.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+
+// include the Direct3D Library file 
+#pragma comment (lib, "d3d11.lib") 
+#pragma comment (lib, "d3dcompiler.lib")
+
+#define SCREEN_WIDTH 800 
+#define SCREEN_HEIGHT 600
+
+// global declarations 
+IDXGISwapChain* swapchain;			// the pointer to the swap chain interface 
+ID3D11Device* dev;					// the pointer to our Direct3D device interface
+ID3D11DeviceContext* devcon;		// the pointer to our Direct3D device context
+ID3D11RenderTargetView* backbuffer;	// the pointer to our back buffer
+ID3D11InputLayout* pLayout;			// the pointer to the input layout
+ID3D11VertexShader* pVS;			// the pointer to the vertex shader
+ID3D11PixelShader* pPS;				// the pointer to the pixel shader
+ID3D11Buffer* pVBuffer;				// the pointer to the vertex buffer
+
+
+typedef float D3DXCOLOR[4]; // RGBA Color
+
+// a struct to define a single vertex
+struct VERTEX
+{
+	FLOAT X, Y, Z;
+	D3DXCOLOR Color;
+};
+
+// function prototypes
+void InitD3D(HWND hWnd);	// sets up and initializes Direct3D
+void RenderFrame(void);		// renders a single frame
+void CleanD3D(void);		// closes Direct3D and releases memory
+void InitGraphics(void);	// creates the shape to render
+void InitPipeline(void);	// loads and prepares the shaders
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+HRESULT CompileShader(_In_ LPCWSTR srcFile, _In_ LPCSTR entryPoint, _In_ LPCSTR profile, _Outptr_ ID3DBlob** blob)
+{
+	if (!srcFile || !entryPoint || !profile || !blob)
+		return E_INVALIDARG;
+
+	*blob = nullptr;
+
+	UINT flags = D3DCOMPILE_ENABLE_STRICTNESS;
+
+#if defined( DEBUG) || defined(_DEBUG)
+	flags |= D3DCOMPILE_DEBUG;
+#endif
+
+	const D3D_SHADER_MACRO defines[] =
+	{
+	"EXAMPLE_DEFINE",
+	"1",
+	NULL,
+	NULL
+	};
+
+	ID3DBlob* shaderBlob = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+	HRESULT hr = D3DCompileFromFile(srcFile,
+		defines,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		entryPoint,
+		profile,
+		flags,
+		0,
+		&shaderBlob,
+		&errorBlob);
+
+	if (FAILED(hr)) {
+		if (errorBlob) {
+			OutputDebugStringA((char*)errorBlob->GetBufferPointer());
+			errorBlob->Release();
+		}
+
+		if (shaderBlob)
+			shaderBlob->Release();
+
+		return hr;
+	}
+
+	*blob = shaderBlob;
+	return hr;
+}
+
+// this function initializes and prepares Direct3D for use 
+void InitD3D(HWND hWnd)
+{
+	// create a struct to hold information about the swap chain
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	// clear out the struct for use
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	// fill the swap chain description struct
+	scd.BufferCount = 1;								// one back buffer
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;	// use 32-bit color
+	scd.BufferDesc.Width = SCREEN_WIDTH;				// set the back buffer width
+	scd.BufferDesc.Height = SCREEN_HEIGHT;				// set the back buffer height
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;	// how swap chain is to be used
+	scd.OutputWindow = hWnd;							// the window to be used 
+	scd.SampleDesc.Count = 4;							// how many multisamples
+	scd.Windowed = TRUE;								// allow windowed/full-screen mode
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;	//full-screen switching
+
+	//create a device, device context and swap chain using the information in the scd struct
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&swapchain,
+		&dev,
+		NULL,
+		&devcon);
+
+	// get the address of the back buffer
+	ID3D11Texture2D* pBackBuffer;
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+	// use the back buffer address to create the render target 
+	dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+	pBackBuffer->Release();
+
+	// set the render target as the back buffer 
+	devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+	// Set the viewport
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = SCREEN_WIDTH;
+	viewport.Height = SCREEN_HEIGHT;
+
+	devcon->RSSetViewports(1, &viewport);
+
+	InitPipeline();
+	InitGraphics();
+}
+
+void InitPipeline()
+{
+	// load and compile the two shaders
+	ID3DBlob* VS, * PS;
+	CompileShader(L"directxShaders.hlsl", "VSMain", "vs_4_0_level_9_1", &VS);
+	CompileShader(L"directxShaders.hlsl", "PSMain", "ps_4_0_level_9_1", &PS);
+
+	// encapsulate both shaders into shader objects
+	dev->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &pVS);
+	dev->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &pPS);
+
+	// set the shader objects
+	devcon->VSSetShader(pVS, 0, 0);
+	devcon->PSSetShader(pPS, 0, 0);
+
+	// create the input layout object
+	D3D11_INPUT_ELEMENT_DESC ied[] =
+	{
+		{"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
+	{"COLOR",0,DXGI_FORMAT_R32G32B32A32_FLOAT,0,12,D3D11_INPUT_PER_VERTEX_DATA,0},
+	};
+
+	dev->CreateInputLayout(ied,
+		2,
+		VS->GetBufferPointer(),
+		VS->GetBufferSize(),
+		&pLayout);
+
+	devcon->IASetInputLayout(pLayout);
+}
+
+// this is the function that creates the shape to render
+void InitGraphics()
+{
+	// create a triangle using the VERTEX struct
+	VERTEX OurVertices[] =
+	{
+		{0.0f, 0.5f, 0.0f,{ 1.0f, 0.0f, 0.0f, 1.0f}},
+		{ 0.45f, -0.5, 0.0f,{0.0f, 1.0f, 0.0f, 1.0f }},
+		{ -0.45f, -0.5f, 0.0f,{0.0f, 0.0f, 1.0f, 1.0f }}
+	};
+
+	// create the vertex buffer
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+
+	bd.Usage = D3D11_USAGE_DYNAMIC;				// write access access by CPU and GPU
+	bd.ByteWidth = sizeof(VERTEX) * 3;			// size is the VERTEX struct * 3
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;	// use as a vertex buffer 
+	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;	// allow CPU to write in buffer
+
+	dev->CreateBuffer(&bd, NULL, &pVBuffer);	// create the buffer
+
+	// copy the vertices into the buffer 
+	D3D11_MAPPED_SUBRESOURCE ms;
+
+	// map the buffer
+	devcon->Map(pVBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);
+	memcpy(ms.pData, OurVertices, sizeof(OurVertices)); // copy the data
+	devcon->Unmap(pVBuffer, NULL);						// unmap the buffer
+}
+
+void RenderFrame(void)
+{
+	// clear the back buffer to a deep blue
+	devcon->ClearRenderTargetView(backbuffer, D3DXCOLOR{ 0.0f, 0.0f, 0.4f, 1.0f });
+
+	// select which vertex buffer to display
+	UINT stride = sizeof(VERTEX);
+	UINT offset = 0;
+	devcon->IASetVertexBuffers(0, 1, &pVBuffer, &stride, &offset);
+
+	// select which printive type we are using
+	devcon->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// draw the vertex buffer to the back buffer 
+	devcon->Draw(3, 0);
+
+	// switch the back buffer and the front buffer 
+	swapchain->Present(0, 0);
+}
+
+void CleanD3D(void)
+{
+	swapchain->SetFullscreenState(FALSE, NULL); // switch to windowed mode
+
+	// close and release all existing COM objects
+	pLayout->Release();
+	pVS->Release();
+	pPS->Release();
+	pVBuffer->Release();
+	swapchain->Release();
+	backbuffer->Release();
+	dev->Release();
+	devcon->Release();
+}
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;						// current instance
+WCHAR szTitle[MAX_LOADSTRING];			// The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];	// the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Place code here.
+
+
+	// Initialize global strings
+	LoadStringW(hInstance,
+		IDS_APP_TITLE,
+		szTitle,
+		MAX_LOADSTRING
+	);
+
+	LoadStringW(hInstance,
+		IDC_LAB2HELLODIRECTX11,
+		szWindowClass,
+		MAX_LOADSTRING
+	);
+
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB2HELLODIRECTX11));
+
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			RenderFrame();
+		}
+	}
+
+
+	// clean up DirectX and COM
+	CleanD3D();
+
+	return (int)msg.wParam;
+
+}
+
+//
+// FUNCTION: MyRegisterClass()
+// //
+// PURPOSE: Registers the window class.
+//﻿
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB2HELLODIRECTX11));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_LAB2HELLODIRECTX11);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+// FUNCTION: InitInstance(HINSTANCE, int)
+//
+// PURPOSE: Saves instance handle and creates main window
+//
+// COMMENTS:
+//
+//In this function, we save the instance handle in a global variable and 
+//create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // Store instance handle in our global variable
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	// set up and initialize Direct3D
+	InitD3D(hWnd);
+
+	return TRUE;
+}
+
+//
+// // FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+// PURPOSE: Processes messages for the main window.
+//
+// WM_COMMAND - process the application menu
+// WM_PAINT - Paint the main window
+// WM_DESTROY - post a quit message and return
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+
+	case WM_PAINT:
+	{
+		//PAINTSTRUCT ps;
+		//HDC hdc = BeginPaint (hWnd, &ps);
+		//// TODO: Add any drawing code that uses hdc here...
+		//EndPaint (hWnd, &ps);
+	}
+	break;
+
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
---
+++ Year_2/Semester_II/SMM - Modern 3D Graphic Modeling Systems/Labs/Lab2/Lab2 - Hello DirectX 9 - Solved/Lab2 - Hello DirectX 9/Lab2 - Hello DirectX 9.cpp
@@ -0,0 +1,267 @@
+#include "framework.h"
+#include "Lab2 - Hello DirectX 9.h"
+
+#include <d3d9.h>
+#pragma comment(lib, "d3d9.lib")
+
+// define the screen resolution #define SCREEN_WIDTH 800 #define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+#define DELTA_WIDTH 80
+#define DELTA_HEIGHT 60
+
+// global declarations
+LPDIRECT3D9 d3d;			// the pointer to our Direct3D interface
+LPDIRECT3DDEVICE9 d3ddev;	// the pointer to the device class
+LPDIRECT3DVERTEXBUFFER9 pVertexBuffer; // the pointer to the vertex buffer 
+
+#define CUSTOMFVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
+
+struct CUSTOMVERTEX		// Custom vertex structure
+{
+	FLOAT x, y, z, rhw;		// from the D3DFVF_XYZRHW flag
+	DWORD color;			// from the D3DFVF_DIFFUSE flag
+};
+
+void InitD3D(HWND hWnd);	// sets up and initializes Direct3D
+void RenderFrame(void);		// renders a single frame
+void CleanD3D(void);		// closes Direct3D and releases memory
+
+void InitD3D(HWND hWnd)
+{
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);		// create the Direct3D interface
+	D3DPRESENT_PARAMETERS d3dpp;				// create a struct to hold various device information
+	ZeroMemory(&d3dpp, sizeof(d3dpp));			// clear out the struct for use
+	d3dpp.Windowed = TRUE;						// program windowed, not fullscreen
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;	// discard old frames
+	d3dpp.hDeviceWindow = hWnd;					// set the window to be used by Direct3D
+
+	// create a device class using this information and information from the d3dpp stuct 
+	d3d->CreateDevice(D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_HAL,
+		hWnd,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+		&d3dpp,
+		&d3ddev);
+
+	d3ddev->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX),
+		0,
+		CUSTOMFVF,
+		D3DPOOL_MANAGED,
+		&pVertexBuffer,
+		NULL);
+
+	RECT windowRect;
+	GetClientRect(hWnd, &windowRect);
+
+	VOID* pVoid; // the void pointer
+	pVertexBuffer->Lock(0, 0, (void**)&pVoid, 0); // lock the vertex buffer 
+	CUSTOMVERTEX pTriangleVertices[] =
+	{
+		//coltul albastru
+		{ SCREEN_WIDTH / 2.0,
+		DELTA_HEIGHT - (DELTA_HEIGHT * 10 / 100),
+		1.0f,
+		1.0f,
+		D3DCOLOR_XRGB(0, 0, 255),
+		},
+
+		//coltul verde
+		{ SCREEN_WIDTH - DELTA_WIDTH,
+		SCREEN_HEIGHT - DELTA_HEIGHT - ((SCREEN_HEIGHT - DELTA_HEIGHT) * 10 / 100),
+		1.0f,
+		1.0f,
+		D3DCOLOR_XRGB(0, 255, 0),
+		},
+
+		//coltul rosu
+		{ DELTA_WIDTH,
+		SCREEN_HEIGHT - DELTA_HEIGHT - ((SCREEN_HEIGHT - DELTA_HEIGHT) * 10 / 100),
+		1.0f,
+		1.0f,
+		D3DCOLOR_XRGB(255, 0, 0),
+		},
+	};
+	memcpy(pVoid, pTriangleVertices, sizeof(pTriangleVertices)); // copy the vertices to the locked buffer
+	pVertexBuffer->Unlock(); // unlock the vertex buffer
+}
+
+void RenderFrame(void)
+{
+	// clear the window to green
+	d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(18, 104, 20), 1.0f, 0);
+
+	d3ddev->BeginScene(); // begins the 3D scene
+	{ // do 3D rendering on the back buffer here
+
+		// select which vertex format we are using 
+		d3ddev->SetFVF(CUSTOMFVF);
+
+		// select the vertex buffer to display
+		d3ddev->SetStreamSource(0, pVertexBuffer, 0, sizeof(CUSTOMVERTEX));
+
+		// copy the vertex buffer to the back buffer
+		d3ddev->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+	}
+	d3ddev->EndScene(); // ends the 3D scene
+
+	d3ddev->Present(NULL, NULL, NULL, NULL); //displays the created frame
+}
+
+void CleanD3D(void)
+{
+	pVertexBuffer->Release();	// close and release the vertex buffer
+	d3ddev->Release();			// close and release the 3D device
+	d3d->Release();				// close and release Direct3D
+}
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;							// current instance
+WCHAR szTitle[MAX_LOADSTRING];				// The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];		// the main window class name
+
+
+//Forward declarations of functions included in this code module:
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int, HWND&);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE			hInstance,
+	_In_opt_ HINSTANCE	hPrevInstance,
+	_In_ LPWSTR			lpCmdLine,
+	_In_ int				nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+
+	// TODO: Place code here.
+
+
+	// Initialize global strings
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_LAB2HELLODIRECTX9, szWindowClass, MAX_LOADSTRING);
+
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	HWND hWnd;
+	if (!InitInstance(hInstance, nCmdShow, hWnd)) {
+		return FALSE;
+	}
+
+	InitD3D(hWnd);
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LAB2HELLODIRECTX9));
+
+	MSG msg;
+
+	// Main message loop:
+	while (GetMessage(&msg, nullptr, 0, 0)) {
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			RenderFrame();
+		}
+	}
+
+	CleanD3D();
+
+	return (int)msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LAB2HELLODIRECTX9)); wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_LAB2HELLODIRECTX9); wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, HWND& hWnd)
+{
+	hInst = hInstance; // Store instance handle in our global variable
+	hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, SCREEN_WIDTH,
+		SCREEN_HEIGHT, nullptr, nullptr, hInstance, nullptr);
+	if (!hWnd) {
+		return FALSE;
+	}
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	return TRUE;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message) {
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId) {
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+			break;
+		}
+		break;
+	}
+
+	case WM_PAINT:
+	{
+		//PAINTSTRUCT ps;
+		//HDC hdc = BeginPaint (hWnd, &ps);
+		//// TODO: Add any drawing code that uses hdc here... 
+		//EndPaint (hWnd, &ps);
+	}
+	break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message) {
+	case WM_INITDIALOG:
+		return(INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
