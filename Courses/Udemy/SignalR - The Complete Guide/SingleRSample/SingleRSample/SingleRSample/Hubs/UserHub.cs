c6c9513f972be04127174b25948b7477e0b57c7b
Denisa-Georgiana Vasile <vasile.denisa.24@gmail.com>
2023-12-10 17:58:07 +0200

    added first SignalR app

---
+++ Courses/Udemy/SignalR - The Complete Guide/SingleRSample/SingleRSample/SingleRSample/Hubs/UserHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SingleRSample.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
+
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+
+            //send update to all clients that total views have been updated
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+    }
+}
---
+++ Courses/Udemy/SignalR - The Complete Guide/SingleRSample/SingleRSample/SingleRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,30 @@
+﻿// create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/user").build();
+
+// connect to methods that hub invokes aka receive notifications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+// invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+// start connection
+function fulfilled() {
+    console.log("Connection to User Hub Successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    // rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
